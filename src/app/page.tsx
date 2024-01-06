65f88eba35f4fd4e4e45ab9e2df8d6e98c676a04
Igor Amon <48227869+amonigor@users.noreply.github.com>
2024-01-05 21:13:43 -0300

    added eslint/prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <p className='font-bold text-5xl'>Santos Stats</p>
+      <p className="text-5xl font-bold">Santos Stats</p>
     </main>
-  )
+  );
 }
